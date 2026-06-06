ca9061ae27493a33133ad0dedbf0235ec06257a3
ale-1988 <alejandrogaut@gmail.com>
2026-06-05 22:58:23 -0300

    Corrigiendo settings

--- manager_repuestos/settings.py
+++ manager_repuestos/settings.py
@@ -2,9 +2,18 @@ import os
 
 from .base_settings import *
 
-ENTORNO = os.environ.get("MR_ENTORNO", "DEV")
+ENTORNO = os.environ.get("MR_ENTORNO")
 
-if ENTORNO.upper() == "PROD":
+if ENTORNO is None:
+    raise RuntimeError(
+        "La variable de entorno MR_ENTORNO no está definida"
+    )
+
+ENTORNO = ENTORNO.upper()
+
+
+if ENTORNO == "PROD":
     from .prod_settings import *
 else:
     from .dev_settings import *
+
